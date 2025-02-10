442413ea731217556e5a676748fd856a4562397c
yqq <youngqqcn@gmail.com>
2025-02-10 15:38:38 +0800

    update

--- OpenAI/gpt/OpenAI_GPT_API.py
+++ OpenAI/gpt/OpenAI_GPT_API.py
@@ -149,6 +149,7 @@ def OpenAI_GPT_API_thread(
         reT += 1
 
         # 暂时不请求deepseek
+        print('暂时不请求deepseek, 直接返回')
         ans.put("Hello, I'm Cami, nice to meet you. I'm testing now.")
         return
 
